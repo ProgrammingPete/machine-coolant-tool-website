ecf6ab1e630a710c3577bb4f1f81ce855be1b747
ProgrammingPete <peter.parianos@yahoo.com>
2024-01-28 11:59:31 -0500

    typo

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -18,7 +18,7 @@ const Portfolio = () => {
                         <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3 min-h-max">
                         <div className="m-2 text-sm">
                                     <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img1} />
-                                    <h2 className="font-semibold my-4 text-xl text-center ">Pressure Regulating Valve </h2>
+                                    <h2 className="font-semibold my-4 text-xl text-center ">Pressure Regulating Valve</h2>
                                     <p className="text-md font-medium">
                                     Fluid enters one side and exits the other through a valve with a spring-loaded plunger that seals a hole in the valve seat. The user can adjust the plunger force by compressing a spring. Fluid only bypasses the system when its pressure exceeds the spring pressure, ensuring a constant maximum pressure, regardless of tool size. The excess flow for smaller tools bypasses back to the sump.
                                     </p>
@@ -27,9 +27,10 @@ const Portfolio = () => {
                         <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
                         <div className="m-2 text-sm">
                                     <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img2} />
-                                    <h2 className="font-semibold my-4 text-xl text-center ">Hyrdra-Cell Accessories</h2>
+                                    <h2 className="font-semibold my-4 text-xl text-center ">Hydra-Cell Accessories</h2>
                                     <p className="text-md font-medium">
-                                    As part of our “Mass Customization” approach, Hydra-Cell pumps are available with a variety of in-stock accessories and options to provide a pumping system ideally suited to your installation and application.                                    </p>
+                                        As part of our “Mass Customization” approach, Hydra-Cell pumps are available with a variety of in-stock accessories and options to provide a pumping system ideally suited to your installation and application.
+                                    </p>
                                 </div>
                         </div>
 
@@ -38,7 +39,7 @@ const Portfolio = () => {
                                     <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img3} />
                                     <h2 className="font-semibold my-4 text-xl text-center ">Metering Accessories</h2>
                                     <p className="text-md font-medium">
-                                    Hydra-Cell metering pumps are just one facet of a complete Hydra-Cell “Metering Solutions” system.We can furnish all components in your pumping system, individually tailored to your specific processing needs.                
+                                        Hydra-Cell metering pumps are just one facet of a complete Hydra-Cell “Metering Solutions” system.We can furnish all components in your pumping system, individually tailored to your specific processing needs.                
                                     </p>
                                 </div>
                         </div>
@@ -47,12 +48,8 @@ const Portfolio = () => {
                             <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img4} />
                                     <h2 className="font-semibold my-4 text-xl text-center ">D15 Metallic (US)</h2>
                                     <p className="text-md font-medium">
-                                        Versatile, Reliable Pumps for a Wide Range of Applications
-
-                                        Pumps the full spectrum of low-to-high viscosity fluids.
-                                        Features a seal-less design and horizontal disk check valves that enable the pump to handle abrasives and particulates that might damage or destroy other types of pumps.
-                                        Simple, compact design reduces initial investment and lowers maintenance costs.
-                                     </p>
+                                        Versatile, Reliable Pumps for a Wide Range of Applications. Pumps the full spectrum of low-to-high viscosity fluids. Features a seal-less design and horizontal disk check valves that enable the pump to handle abrasives and particulates that might damage or destroy other types of pumps. Simple, compact design reduces initial investment and lowers maintenance costs.
+                                    </p>
                             </div>
                         </div>               
                     </div>
